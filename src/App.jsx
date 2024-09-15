73ca3575d11abb4282f978a5f9b9d0ea1a896635
sabiha-wasema <sabihawasemaa1244@gmail.com>
2024-09-15 21:09:24 +0600

    all calculation part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,93 @@
-import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+  const [reportData, setReportData] = useState(null);
+  const [topPurchasers, setTopPurchasers] = useState(null);
+  const [grossTotal, setGrossTotal] = useState(null);
+  const [grossQuantity, setGrossQuantity] = useState(null);
+  const [totalTopPrice, setTotalTopPrice] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchingData = async () => {
-      try {
-        setLoading(true);
-        setError("");
-        const response = await axios.get(
-          "https://raw.githubusercontent.com/Bit-Code-Technologies/mockapi/main/purchase.json"
-        );
-        const fetchedData = response.data;
-        // setData(fetchedData);
-        const processedReport = fetchedData?.map((item) => ({
-          productName: item?.product_name,
-          customerName: item?.name,
-          quantity: item?.purchase_quantity,
-          price: parseFloat(item?.product_price),
-          total: item?.purchase_quantity * parseFloat(item?.product_price),
-        }));
+  const fetchData = async () => {
+    setLoading(true);
+    setError(null);
+    setTopPurchasers(null);
+    try {
+      const response = await axios.get("http://localhost:5000/fetch-and-store");
+      setReportData(response.data);
+    } catch (err) {
+      setError("Error fetching data from the server");
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-        const grossQuantity = processedReport.reduce(
-          (acc, item) => acc + item?.quantity,
-          0
-        );
-        const totalPrice = processedReport.reduce(
-          (acc, item) => acc + item?.price,
-          0
-        )
-        const grossTotal = processedReport.reduce(
-          (acc, item) => acc + item?.total,
-          0
-        );
+  const fetchTopPurchasers = async () => {
+    setLoading(true);
+    setError(null);
+    setReportData(null);
+    try {
+      const response = await axios.get("http://localhost:5000/top-purchasers");
+      setTopPurchasers(response.data.topPurchasers);
+      setGrossTotal(response.data.grossTotal);
+      setGrossQuantity(response.data.grossQuantity);
+      setTotalTopPrice(response.data.totalPrice);
+    } catch (err) {
+      setError("the error while fetching top purchasers from the server");
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-        setData({
-          items: processedReport,
-          gross: { quantity: grossQuantity, total: grossTotal , price:totalPrice},
-        });
-      } catch (err) {
-        setError("Error while fetching data");
-        console.error(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchingData();
-  }, []);
+  const resetData = () => {
+    setReportData(null);
+    setTopPurchasers(null);
+    setGrossTotal(null);
+    setGrossQuantity(null);
+    setTotalTopPrice(null);
+  };
+
   return (
-    <div className="App p-4 max-w-4xl mx-auto">
+    <div className="p-4 max-w-4xl mx-auto">
       <h1 className="text-2xl font-bold mb-4 text-center">Store Data Report</h1>
+
       {loading && (
-        <div className="flex items-center justify-center min-h-screen">
-          <p className="text-lg font-medium text-blue-600 animate-pulse">
-            Please wait, Data is Loading...
-          </p>
+        <div className="loading-container">
+          <div className="spinner"></div>
+          <p className="loading-text">Please wait, Data is Loading...</p>
         </div>
       )}
       {error && <p className="text-red-500 text-center">{error}</p>}
-      {data?.items && data?.items?.length > 0 && (
-        <div className="overflow-x-auto">
+
+      <div className="text-center mb-4">
+        <button
+          onClick={fetchData}
+          className="custom-linear-gradient text-white px-4 py-2 rounded mr-2"
+        >
+          Generate Report
+        </button>
+        <button
+          onClick={fetchTopPurchasers}
+          className="custom-linear-gradient text-white px-4 py-2 rounded mr-2"
+        >
+          Top Purchasers
+        </button>
+        <button
+          onClick={resetData}
+          className="custom-radial-gradient text-white px-4 py-2 rounded"
+        >
+          Reset
+        </button>
+      </div>
+
+      {reportData?.items && reportData?.items?.length > 0 && (
+        <div className="overflow-x-auto mb-8">
+          <h2 className="text-xl font-bold mb-4">Product Report</h2>
           <table className="min-w-full bg-white border border-gray-200 shadow-md rounded-lg">
             <thead className="bg-gray-300 border-b border-gray-300">
               <tr>
@@ -79,7 +103,7 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              {data.items.map((item, index) => (
+              {reportData?.items.map((item, index) => (
                 <tr
                   key={index}
                   className={`border-b text-left ${
@@ -89,18 +113,87 @@ function App() {
                   <td className="py-2 px-4">{item?.productName}</td>
                   <td className="py-2 px-4">{item?.customerName}</td>
                   <td className="py-2 px-4">{item?.quantity}</td>
-                  <td className="py-2 px-4"><span className="font-semibold">$ </span>{item?.price.toFixed(2)}</td>
-                  <td className="py-2 px-4"><span className="font-semibold">$ </span>{item?.total.toFixed(2)}</td>
+                  <td className="py-2 px-4">
+                    <span className="font-semibold">$</span>{" "}
+                    {item?.price?.toFixed(2)}
+                  </td>
+                  <td className="py-2 px-4">
+                    <span className="font-semibold">$</span>{" "}
+                    {item?.total?.toFixed(2)}
+                  </td>
                 </tr>
               ))}
               <tr>
                 <td colSpan="2" className="py-2 px-4 font-bold text-right">
                   Gross Total:
                 </td>
-                <td className="py-2 px-4 ">{data?.gross?.quantity}</td>
-                <td className="py-2 px-4 "><span className="font-semibold">$ </span>{data?.gross?.price.toFixed(2)}</td>
-                <td className="py-2 px-4 ">
-                <span className="font-semibold">$ </span>{data?.gross?.total.toFixed(2)}
+                <td className="py-2 px-4">{reportData?.gross?.quantity}</td>
+                <td className="py-2 px-4">
+                  <span className="font-semibold">$</span>{" "}
+                  {reportData?.gross?.price?.toFixed(2)}
+                </td>
+                <td className="py-2 px-4">
+                  <span className="font-semibold">$</span>{" "}
+                  {reportData?.gross?.total?.toFixed(2)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+
+      {topPurchasers && topPurchasers?.length > 0 && (
+        <div className="overflow-x-auto">
+          <h2 className="text-xl font-bold mb-4">Top Purchasers Report</h2>
+          <table className="min-w-full bg-white border border-gray-200 shadow-md rounded-lg">
+            <thead className="bg-gray-300 border-b border-gray-300">
+              <tr>
+                <th className="py-3 px-4 text-left text-gray-700">User Name</th>
+                <th className="py-3 px-4 text-left text-gray-700">
+                  Top Product
+                </th>
+                <th className="py-3 px-4 text-left text-gray-700">
+                  Top Quantity
+                </th>
+                <th className="py-3 px-4 text-left text-gray-700">Top Price</th>
+                <th className="py-3 px-4 text-left text-gray-700">
+                  Total Amount Spent
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {topPurchasers?.map((user, index) => (
+                <tr
+                  key={index}
+                  className={`border-b text-left ${
+                    index % 2 === 0 ? "bg-gray-100" : "bg-white"
+                  }`}
+                >
+                  <td className="py-2 px-4">{user?.user_name}</td>
+                  <td className="py-2 px-4">{user?.top_product}</td>
+                  <td className="py-2 px-4">{user?.top_quantity}</td>
+                  <td className="py-2 px-4">
+                    <span className="font-semibold">$</span>{" "}
+                    {user?.top_price?.toFixed(2)}
+                  </td>
+                  <td className="py-2 px-4">
+                    <span className="font-semibold">$</span>{" "}
+                    {user?.total_amount_spent?.toFixed(2)}
+                  </td>
+                </tr>
+              ))}
+              <tr>
+                <td colSpan="2" className="py-2 px-4 font-bold text-right">
+                  Gross Total Spent:
+                </td>
+                <td className="py-2 px-4">{grossQuantity}</td>
+                <td className="py-2 px-4">
+                  <span className="font-semibold">$</span>{" "}
+                  {grossTotal?.toFixed(2)}
+                </td>
+                <td className="py-2 px-4">
+                  <span className="font-semibold">$</span>{" "}
+                  {totalTopPrice?.toFixed(2)}
                 </td>
               </tr>
             </tbody>
